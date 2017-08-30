42bde7879132a678f7fdfd88538dbfadc1fbc31a
Lars <lars.baertschi@bluewin.ch>
2017-08-30 22:24:35 +0200

    + Adapted plugin loader

--- src/main/java/net/twasi/core/plugin/PluginLoader.java
+++ src/main/java/net/twasi/core/plugin/PluginLoader.java
@@ -1,13 +1,24 @@
 package net.twasi.core.plugin;
 
 import net.twasi.core.logger.TwasiLogger;
+import net.twasi.core.plugin.api.TwasiPlugin;
+import net.twasi.core.plugin.api.TwasiPluginInterface;
+import org.yaml.snakeyaml.Yaml;
 
+import javax.annotation.Resource;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PluginLoader {
 
-
+    ArrayList<TwasiPlugin> loadedPlugins = new ArrayList<>();
 
     public PluginLoader() {
 
@@ -23,9 +34,18 @@ public class PluginLoader {
         });
 
         for(File plugin : pluginJars) {
-            System.out.println(plugin.getAbsolutePath());
-        }
+            TwasiLogger.log.debug("Loading plugin " + plugin);
 
+            try {
+                TwasiPluginLoader urlCl = new TwasiPluginLoader(System.class.getClassLoader(), plugin);
+                loadedPlugins.add(urlCl.plugin);
+            } catch (ClassNotFoundException e) {
+                TwasiLogger.log.error(e);
+            } catch (Exception e) {
+                TwasiLogger.log.error(e);
+            }
+        }
+        TwasiLogger.log.info(loadedPlugins.size() + " plugins loaded.");
     }
 
 }
---
+++ src/main/java/net/twasi/core/plugin/TwasiPluginLoader.java
@@ -0,0 +1,69 @@
+package net.twasi.core.plugin;
+
+import net.twasi.core.logger.TwasiLogger;
+import net.twasi.core.plugin.api.TwasiPlugin;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
+
+final public class TwasiPluginLoader extends URLClassLoader {
+        private final Map<String, Class<?>> classes = new HashMap<String, Class<?>>();
+        private final File file;
+        final TwasiPlugin plugin;
+        private TwasiPlugin pluginInit;
+        private IllegalStateException pluginState;
+
+        TwasiPluginLoader(final ClassLoader parent, final File file) throws Exception, MalformedURLException {
+            super(new URL[]{file.toURI().toURL()}, parent);
+            this.file = file;
+
+            // Check for plugin.yml
+            URL infoYamlUrl = this.getResource("plugin.yml");
+            if (infoYamlUrl == null) {
+                throw new Exception("Cannot load plugin " + file.getAbsolutePath() + ": Invalid or non-existent plugin.yml");
+            }
+
+            // Get File, read
+            InputStream stream = (InputStream) infoYamlUrl.getContent();
+            Yaml yaml = new Yaml();
+            HashMap config = yaml.loadAs( stream, HashMap.class);
+            String main = (String) config.get("Main");
+
+            try {
+                Class<?> jarClass;
+                try {
+                    jarClass = Class.forName(main, true, this);
+                } catch (ClassNotFoundException ex) {
+                    throw new Exception("Cannot find main class `" + main + "'", ex);
+                }
+
+                Class<? extends TwasiPlugin> pluginClass;
+                try {
+                    pluginClass = jarClass.asSubclass(TwasiPlugin.class);
+                } catch (ClassCastException ex) {
+                    throw new Exception("main class `" + main + "' does not extend TwasiPlugin", ex);
+                }
+
+                plugin = pluginClass.newInstance();
+            } catch (IllegalAccessException ex) {
+                throw new Exception("No public constructor", ex);
+            } catch (InstantiationException ex) {
+                throw new Exception("Abnormal plugin type", ex);
+            }
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            try {
+                return Class.forName(name);
+            } catch (Exception e) {
+                return super.findClass(name);
+            }
+        }
+}
