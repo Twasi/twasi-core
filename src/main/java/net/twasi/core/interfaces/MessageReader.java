2a0f8c2c7a2e3b1a04b558f54c16f99e7f4db67b
Lars Bärtschi <lars.baertschi@bluewin.ch>
2018-01-19 21:30:11 +0100

    + Added correct ping answers

--- src/main/java/net/twasi/core/interfaces/MessageReader.java
+++ src/main/java/net/twasi/core/interfaces/MessageReader.java
@@ -2,6 +2,7 @@ package net.twasi.core.interfaces;
 
 import net.twasi.core.interfaces.api.TwasiInterface;
 import net.twasi.core.logger.TwasiLogger;
+import net.twasi.core.models.Message.MessageType;
 import net.twasi.core.models.Message.TwasiMessage;
 
 public class MessageReader implements Runnable {
@@ -19,7 +20,12 @@ public class MessageReader implements Runnable {
                 if (message == null) {
                     continue;
                 }
-                // TwasiLogger.log.info(message.getMessage() + ", " + message.getType() + ", " + message.getSender());
+                TwasiLogger.log.trace("IRC: message=" + message.getMessage() + ", type=" + message.getType() + ", sender=" + message.getSender());
+                if (message.getType().equals(MessageType.PING)) {
+                    twasiInterface.getCommunicationHandler().sendRawMessage("PONG " + message.getMessage());
+                    TwasiLogger.log.debug("PING answered: " + message.getMessage());
+                    continue;
+                }
                 twasiInterface.getDispatcher().dispatch(message);
             } catch (Throwable e) {
                 TwasiLogger.log.error("Exception in MessageReader of " + twasiInterface.getStreamer().getUser().getTwitchAccount().getUserName() + ": " + e.getMessage());
--- src/main/java/net/twasi/core/interfaces/twitch/TwitchCommunicationHandler.java
+++ src/main/java/net/twasi/core/interfaces/twitch/TwitchCommunicationHandler.java
@@ -9,45 +9,49 @@ import java.io.IOException;
 
 public class TwitchCommunicationHandler extends CommunicationHandler {
 
-    public TwitchCommunicationHandler(TwitchInterface inf) {
+    TwitchCommunicationHandler(TwitchInterface inf) {
         super(inf);
     }
 
     @Override
-        public boolean sendMessage(String message) {
-            TwitchInterface twitchInterface = (TwitchInterface) getInterface();
+    public boolean sendMessage(String message) {
+            return sendRawMessage("PRIVMSG " + getInterface().getStreamer().getUser().getTwitchAccount().getChannel() + " :" + message);
+    }
 
-            try {
-                twitchInterface.getWriter().write("PRIVMSG " + getInterface().getStreamer().getUser().getTwitchAccount().getChannel() + " :" + message + "\n");
-                twitchInterface.getWriter().flush();
-                return true;
-            } catch (IOException e) {
-                TwasiLogger.log.error(e);
-                e.printStackTrace();
-                return false;
-            }
+    @Override
+    public boolean sendRawMessage(String rawMessage) {
+        try {
+            TwitchInterface twitchInterface = (TwitchInterface) getInterface();
+            twitchInterface.getWriter().write(rawMessage + "\n");
+            twitchInterface.getWriter().flush();
+            return true;
+        } catch (IOException e) {
+            TwasiLogger.log.error(e);
+            e.printStackTrace();
+            return false;
         }
+    }
 
-        @Override
-        public TwasiMessage readMessage() {
-            TwitchInterface twitchInterface = (TwitchInterface) getInterface();
+    @Override
+    public TwasiMessage readMessage() {
+        TwitchInterface twitchInterface = (TwitchInterface) getInterface();
 
-            try {
-                String line = twitchInterface.getReader().readLine();
-                if (line == null) {
-                    return null;
-                }
-                return TwasiMessage.parse(line, getInterface());
-            } catch (IOException e) {
-                if (e.getMessage().equals("Socket closed")) {
-                    TwasiLogger.log.info("Connection to Socket lost for Interface " + twitchInterface.getStreamer().getUser().getTwitchAccount().getUserName());
-                    return null;
-                }
-
-                TwasiLogger.log.error(e);
-                e.printStackTrace();
+        try {
+            String line = twitchInterface.getReader().readLine();
+            if (line == null) {
                 return null;
             }
+            return TwasiMessage.parse(line, getInterface());
+        } catch (IOException e) {
+            if (e.getMessage().equals("Socket closed")) {
+                TwasiLogger.log.info("Connection to Socket lost for Interface " + twitchInterface.getStreamer().getUser().getTwitchAccount().getUserName());
+                return null;
+            }
+
+            TwasiLogger.log.error(e);
+            e.printStackTrace();
+            return null;
         }
+    }
 
 }
