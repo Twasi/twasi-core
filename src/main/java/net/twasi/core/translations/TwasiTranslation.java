d87358cdc43e15bb9d333c975a3348f2562b07df
DESKTOP-AFG2T2T\DieserMerlin <email@merlinw.de>
2019-01-16 21:38:23 +0100

    + random translations (multiple translations with shared key)

--- src/main/java/net/twasi/core/translations/TwasiTranslation.java
+++ src/main/java/net/twasi/core/translations/TwasiTranslation.java
@@ -5,6 +5,8 @@ import net.twasi.core.database.models.User;
 import net.twasi.core.logger.TwasiLogger;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class TwasiTranslation {
@@ -15,6 +17,14 @@ public class TwasiTranslation {
     }
 
     public String getTranslation(Language language, String translationKey) {
+        return getTranslation(language, translationKey, false);
+    }
+
+    public String getRandomTranslation(Language language, String translationKey) {
+        return getTranslation(language, translationKey, true);
+    }
+
+    private String getTranslation(Language language, String translationKey, boolean random) {
 
         if (language == null) {
             language = Language.EN_GB;
@@ -31,6 +41,19 @@ public class TwasiTranslation {
             return getTranslation(Language.EN_GB, translationKey);
         }
 
+        String result = random ? readRandomTranslation(inputStream, translationKey) : readTranslation(inputStream, translationKey);
+        if (result != null) return result;
+
+        if (language == Language.EN_GB) {
+            TwasiLogger.log.warn("Unknown translation key '" + translationKey + "' searched, but not found. This may lead to unexpected results.");
+            return translationKey;
+        }
+
+        // Default to english if nothing was found
+        return getTranslation(Language.EN_GB, translationKey);
+    }
+
+    private String readTranslation(InputStream inputStream, String translationKey) {
         try (Scanner scanner = new Scanner(inputStream)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -42,14 +65,27 @@ public class TwasiTranslation {
             TwasiLogger.log.error(e);
             e.printStackTrace();
         }
+        return null;
+    }
 
-        if (language == Language.EN_GB) {
-            TwasiLogger.log.warn("Unknown translation key '" + translationKey + "' searched, but not found. This may lead to unexpected results.");
-            return translationKey;
+    private String readRandomTranslation(InputStream inputStream, String translationKey) {
+        ArrayList<String> matching = new ArrayList<>();
+        try (Scanner scanner = new Scanner(inputStream)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith(translationKey + "=")) {
+                    matching.add(line.substring(translationKey.length() + 1));
+                }
+            }
+            if (matching.size() > 0) {
+                Collections.shuffle(matching);
+                return matching.get(0);
+            }
+        } catch (Exception e) {
+            TwasiLogger.log.error(e);
+            e.printStackTrace();
         }
-
-        // Default to english if nothing was found
-        return getTranslation(Language.EN_GB, translationKey);
+        return null;
     }
 
     public String getTranslation(User user, String translationKey) {
@@ -58,5 +94,13 @@ public class TwasiTranslation {
 
     public String getTranslation(User user, String translationKey, Object... objects) {
         return String.format(getTranslation(user, translationKey), objects);
+    }
+
+    public String getRandomTranslation(User user, String translationKey) {
+        return getRandomTranslation(user.getConfig().getLanguage(), translationKey);
+    }
+
+    public String getRandomTranslation(User user, String translationKey, Object... objects) {
+        return String.format(getRandomTranslation(user, translationKey), objects);
     }
 }
