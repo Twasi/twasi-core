1ef3d3cae4070328784215e91deec8603686f0af
DESKTOP-AFG2T2T\DieserMerlin <email@merlinw.de>
2019-01-26 10:21:36 +0100

    + debug logging for TwitchInterfaceMaintainer

--- src/main/java/net/twasi/core/interfaces/twitch/TwitchInterfaceMaintainer.java
+++ src/main/java/net/twasi/core/interfaces/twitch/TwitchInterfaceMaintainer.java
@@ -10,6 +10,7 @@ public class TwitchInterfaceMaintainer extends Thread {
     private TwitchInterface twitchInterface;
 
     public TwitchInterfaceMaintainer(TwitchInterface twitchInterface) {
+        TwasiLogger.log.debug("TwitchInterfaceMaintainer registered for user " + twitchInterface.getStreamer().getUser().getId());
         this.twitchInterface = twitchInterface;
         this.setDaemon(true); // No need to wait for a maintainer when a Twasi-stop is requested
     }
@@ -19,6 +20,7 @@ public class TwitchInterfaceMaintainer extends Thread {
         while (true) {
             if (!run) return;
             if (!twitchInterface.getBot().isConnected()) {
+                TwasiLogger.log.debug("TwitchInterfaceMaintainer for user " + twitchInterface.getStreamer().getUser().getId() + " detected an unconnected TwitchInterface");
                 try {
                     twitchInterface.getBot().startBot();
                     TimeUnit.SECONDS.sleep(10);
@@ -30,6 +32,7 @@ public class TwitchInterfaceMaintainer extends Thread {
     }
 
     public void stopMaintainer() {
+        TwasiLogger.log.debug("TwitchInterfaceMaintainer stopped for user " + twitchInterface.getStreamer().getUser().getId());
         run = false;
     }
 }
