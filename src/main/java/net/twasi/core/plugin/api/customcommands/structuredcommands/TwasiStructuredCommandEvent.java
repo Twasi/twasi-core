b61bff789fcfb821a2f64a46791643e3f180152c
Merlin Westphal <email@merlinw.de>
2019-05-27 11:19:57 +0200

    Fixed StruturedCommands

--- src/main/java/net/twasi/core/plugin/api/customcommands/structuredcommands/TwasiStructuredCommandEvent.java
+++ src/main/java/net/twasi/core/plugin/api/customcommands/structuredcommands/TwasiStructuredCommandEvent.java
@@ -8,21 +8,23 @@ import java.util.List;
 public class TwasiStructuredCommandEvent extends TwasiCustomCommandEvent {
 
     private List<String> args;
-    private String baseCommand;
+    private String baseCommand = null;
 
     public TwasiStructuredCommandEvent(TwasiCustomCommandEvent event) {
         super(event.getCommand(), event.getLoader());
-        this.args = event.getArgs();
-        this.baseCommand = args.get(0).toLowerCase();
-        args.remove(0);
+        args = event.getArgs();
+        try {
+            this.baseCommand = args.get(0).toLowerCase();
+            args.remove(0);
+        } catch (Throwable ignored) {
+            // Subcommand end
+        }
     }
 
     @Override
     public List<String> getArgs() {
         try {
-            List<String> a = new ArrayList<>(args);
-            a.remove(0);
-            return a;
+            return new ArrayList<>(args);
         } catch (Exception e) {
             return new ArrayList<>();
         }
