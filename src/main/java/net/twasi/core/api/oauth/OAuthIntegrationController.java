f46718d756319a5cc722dbbaf0f6676a97e46d68
Merlin Westphal <email@merlinw.de>
2019-07-08 14:12:36 +0200

    + OAuth redirection improvements

--- src/main/java/net/twasi/core/api/oauth/OAuthIntegrationController.java
+++ src/main/java/net/twasi/core/api/oauth/OAuthIntegrationController.java
@@ -7,7 +7,6 @@ import net.twasi.core.logger.TwasiLogger;
 import net.twasi.core.services.IService;
 import net.twasi.core.services.ServiceRegistry;
 import net.twasi.core.services.providers.JWTService;
-import net.twasi.core.services.providers.config.ConfigService;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import javax.servlet.ServletException;
@@ -30,6 +29,10 @@ public class OAuthIntegrationController extends HttpServlet implements IService 
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String redirectUri = req.getParameter("environment");
+        if (redirectUri == null)
+            redirectUri = "https://panel.twasi.net";
+
         try {
             String[] parts = req.getPathInfo().substring(1).split("/"); // Substring to remove leading slash
             String oauthService = parts[0];
@@ -60,16 +63,11 @@ public class OAuthIntegrationController extends HttpServlet implements IService 
 
             resp.sendRedirect(handler.getOauthUri(state));
 
-            String redirectUri = req.getParameter("environment");
-            String finalRedirectUri = redirectUri;
-            if (redirectUri == null || ConfigService.get().getCatalog().auth.endpoints.stream().noneMatch(ep -> ep.equalsIgnoreCase(finalRedirectUri)))
-                redirectUri = "https://panel.twasi.net";
-
             this.states.put(state, new OAuthState(user, redirectUri));
         } catch (Exception e) {
             TwasiLogger.log.warn("OAuth request could not be handled");
             TwasiLogger.log.debug("OAuth request failed", e);
-            resp.sendRedirect(ConfigService.get().getCatalog().webinterface.self);
+            resp.sendRedirect(redirectUri);
         }
     }
 
