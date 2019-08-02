13e8d91cb23b13440749e2185ace4e69bc1fb58f
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-08-02 22:35:45 +0200

    Add telegram integration to support, disallow only whitespace messages

--- src/main/java/net/twasi/core/graphql/GraphQLExceptionHandler.java
+++ src/main/java/net/twasi/core/graphql/GraphQLExceptionHandler.java
@@ -10,7 +10,9 @@ import graphql.language.SourceLocation;
 import net.twasi.core.logger.TwasiLogger;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GraphQLExceptionHandler implements DataFetcherExceptionHandler {
     @Override
@@ -25,16 +27,23 @@ public class GraphQLExceptionHandler implements DataFetcherExceptionHandler {
             return DataFetcherExceptionHandlerResult.newResult(handledException.getGraphQLException(sourceLocation, path)).build();
         }
 
-        TwasiLogger.log.error("Data fetcher failed with unhandled exception.", exception);
+        TwasiLogger.log.debug("Data fetcher failed with unhandled exception.", exception);
         return DataFetcherExceptionHandlerResult.newResult(new GraphQLError() {
             @Override
             public String getMessage() {
-                return exception.getMessage();
+                return "Unknown error occurred.";
             }
 
             @Override
             public List<SourceLocation> getLocations() {
                 return Collections.singletonList(sourceLocation);
+            }
+
+            @Override
+            public Map<String, Object> getExtensions() {
+                HashMap<String, Object> values = new HashMap<>();
+                values.put("localisedKey", "error.unknown");
+                return values;
             }
 
             @Override
--- src/main/java/net/twasi/core/graphql/model/support/SupportDTO.java
+++ src/main/java/net/twasi/core/graphql/model/support/SupportDTO.java
@@ -3,11 +3,14 @@ package net.twasi.core.graphql.model.support;
 import net.twasi.core.database.models.User;
 import net.twasi.core.database.models.UserRank;
 import net.twasi.core.database.models.support.SupportTicket;
+import net.twasi.core.database.models.support.SupportTicketMessage;
 import net.twasi.core.database.models.support.SupportTicketType;
 import net.twasi.core.database.repositories.SupportTicketRepository;
 import net.twasi.core.graphql.TwasiGraphQLHandledException;
 import net.twasi.core.services.ServiceRegistry;
 import net.twasi.core.services.providers.DataService;
+import net.twasi.core.services.providers.TelegramService;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,15 +50,30 @@ public class SupportDTO {
             throw new TwasiGraphQLHandledException("This category type is not available.", "support.validation.invalidCategory");
         }
 
+        topic = topic.trim();
         if (topic.isEmpty()) {
             throw new TwasiGraphQLHandledException("The topic may not be empty.", "support.validation.emptyTopic");
         }
 
+        message = message.trim();
         if (message.isEmpty()) {
             throw new TwasiGraphQLHandledException("The message may not be empty.", "support.validation.emptyMessage");
         }
 
         SupportTicket ticket = repository.create(user, topic, message, categoryType);
+        List<SupportTicketMessage> messages = ticket.getEntries();
+        SupportTicketMessage initial = ticket.getEntries().get(0);
+
+        TelegramService telegram = TelegramService.get();
+        if(telegram.isConnected()) {
+            try {
+                telegram.sendMessageToTelegramChat(
+                        "(☞ﾟヮﾟ)☞ Neues Supportticket: #" +
+                                ticket.getId() + ", User: " + ticket.getOwner().getTwitchAccount().getDisplayName() + ", Bereich: " + ticket.getCategory() + ", " + ticket.getTopic() + " - " + initial.getMessage().substring(0, Math.min(initial.getMessage().length(), 100))
+                );
+            } catch (TelegramApiException ignored) {
+            }
+        }
 
         return new SupportTicketDTO(ticket);
     }
@@ -83,11 +101,26 @@ public class SupportDTO {
             throw new TwasiGraphQLHandledException("The ticket you tried to edit does not exist.", null);
         }
 
+        message = message.trim();
+
         if (message.isEmpty()) {
             throw new TwasiGraphQLHandledException("The message may not be empty.", "support.validation.emptyMessage");
         }
 
         repository.addReply(ticket, user, isAdminContext, message, close);
+
+        if (!isAdminContext) {
+            TelegramService telegram = TelegramService.get();
+            if(telegram.isConnected()) {
+                try {
+                    telegram.sendMessageToTelegramChat(
+                            "(☞ﾟヮﾟ)☞ Neues Antwort auf ein Supportticket: #" +
+                                    ticket.getId() + ", User: " + user.getTwitchAccount().getDisplayName() + "; " + message.substring(0, Math.min(message.length(), 100))
+                    );
+                } catch (TelegramApiException ignored) {
+                }
+            }
+        }
 
         ticket = repository.getById(ticket.getId());
         return new SupportTicketDTO(ticket);
