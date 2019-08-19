0e20016839fb90bf750951ff03845dd4bff9b7bf
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-08-19 20:27:46 +0200

    add all entries to beta code to not accidentally delete any

--- src/main/java/net/twasi/core/database/models/BetaCode.java
+++ src/main/java/net/twasi/core/database/models/BetaCode.java
@@ -1,5 +1,6 @@
 package net.twasi.core.database.models;
 
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
 
 import java.util.Date;
@@ -9,6 +10,9 @@ public class BetaCode extends BaseEntity {
     private String betaCode;
     private Date unlocked;
     private String userId;
+    private String email;
+    private Date joined;
+    private ObjectId objectId;
 
     public String getBetaCode() {
         return betaCode;
@@ -36,5 +40,29 @@ public class BetaCode extends BaseEntity {
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getJoined() {
+        return joined;
+    }
+
+    public void setJoined(Date joined) {
+        this.joined = joined;
+    }
+
+    public ObjectId getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(ObjectId objectId) {
+        this.objectId = objectId;
     }
 }
