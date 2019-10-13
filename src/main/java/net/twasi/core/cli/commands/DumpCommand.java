7f83b497994fb8207183d0309a8becee48dda058
Merlin Westphal <email@merlinw.de>
2019-10-13 22:31:21 +0200

    + CLI improvements  - removed command prompt (buggy)  - made CLI modular (plugins can register CLI-commands now)  - CLI code cleanup

---
+++ src/main/java/net/twasi/core/cli/commands/DumpCommand.java
@@ -0,0 +1,27 @@
+package net.twasi.core.cli.commands;
+
+import net.twasi.core.cli.TwasiCLICommand;
+
+import java.util.List;
+
+public class DumpCommand extends TwasiCLICommand {
+    @Override
+    public String getCommandName() {
+        return "dump";
+    }
+
+    @Override
+    public String getCommandHelptext() {
+        return "Not implemented yet.";
+    }
+
+    @Override
+    public String getCommandDescription() {
+        return "Not implemented yet.";
+    }
+
+    @Override
+    public String execute(List<String> args) {
+        return "Not implemented yet.";
+    }
+}
