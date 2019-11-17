c6e756d5c85d226e4ffb6b199d2ad71ad109f4b0
Merlin Westphal <email@merlinw.de>
2019-11-17 23:09:57 +0100

    + Way more flexible publish methods for TwasiWebsocketListenerEndpoint

--- src/main/java/net/twasi/core/api/ws/TwasiWebsocketListenerEndpoint.java
+++ src/main/java/net/twasi/core/api/ws/TwasiWebsocketListenerEndpoint.java
@@ -5,6 +5,8 @@ import net.twasi.core.api.ws.models.TwasiWebsocketClient;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public abstract class TwasiWebsocketListenerEndpoint extends TwasiWebsocketEndpoint {
 
@@ -24,6 +26,22 @@ public abstract class TwasiWebsocketListenerEndpoint extends TwasiWebsocketEndpo
 
     protected final void publish(String s) {
         listeners.forEach(l -> l.getConnection().send(s));
+    }
+
+    protected final void publish(Stream<TwasiWebsocketClient> stream, JsonElement ob) {
+        publish(stream, ob.toString());
+    }
+
+    protected final void publish(Stream<TwasiWebsocketClient> stream, String s) {
+        stream.forEach(l -> l.getConnection().send(s));
+    }
+
+    protected final void publish(Predicate<TwasiWebsocketClient> filter, JsonElement ob) {
+        publish(filter, ob.toString());
+    }
+
+    protected final void publish(Predicate<TwasiWebsocketClient> filter, String s) {
+        publish(listeners.stream().filter(filter), s);
     }
 
     public boolean allowAnonymousListening() {
