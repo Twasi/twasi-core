f9aa445f2f58e5eeac86bba633b4690c778cf887
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-12-06 10:04:29 +0100

    attempt to set the git tag

--- ci/build.sh
+++ ci/build.sh
@@ -1,3 +1,29 @@
 sed -i "s/LIVE/$GITHUB_SHA/g" pom.xml
 mvn -q -B -s maven-settings.xml clean compile assembly:single deploy
-mvn help:evaluate -Dexpression=project.version -q -DforceStdout
+
+# get current version
+t=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+# if we have a blank tag, get all commits
+if [ -z "$t" ]
+then
+    log=$(git log --pretty=oneline)
+    t=0.0.0
+# otherwise, get commits since last tag
+else
+    log=$(git log $t..HEAD --pretty=oneline)
+fi
+
+# supports #major, #minor, #patch (anything else will be 'minor')
+case "$log" in
+    *#major* ) new=$(semver bump major $t);;
+    *#patch* ) new=$(semver bump patch $t);;
+    *#minor* ) new=$(semver bump minor $t);;
+esac
+
+# get current commit hash
+commit=$(git rev-parse HEAD)
+
+git tag -a $new -m "new version $new" $commit
+
+git show $new
